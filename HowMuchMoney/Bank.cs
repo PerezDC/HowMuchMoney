f428eca17dc83b652d16b1b2f4932f8b7a14d804
PerezDC <daviidcp@gmail.com>
2020-08-04 08:40:38 -0700

    Add project files.

---
+++ HowMuchMoney/Bank.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HowMuchMoney
+{
+    public class Bank
+    {
+        static string[] coins = { "penny", "nickel", " dime", "quarter" };
+        Random RandomNumber;
+
+        public Bank()
+        {
+            RandomNumber = new Random();
+        }
+
+        public Bank(int RandomSeed)
+        {
+            RandomNumber = new Random(RandomSeed);
+        }
+
+        public int CountMyCoins()
+        {
+            return RandomNumber.Next(10, 50);
+        }
+
+        public string[] GetCoins(int howMany)
+        {
+            string[] myCoins = new string[howMany];
+
+            for (int i = 0; i < howMany; i++)
+            {
+                myCoins[i] = coins[RandomNumber.Next(0, 4)];
+                Console.Write(myCoins[i] + ' ');
+            }
+            Console.WriteLine();
+            return myCoins;
+        }
+
+        public int HowMuch( string[] allTheCoins)
+        {
+            int total = 0;
+
+            return total;
+        }
+    }
+}
---
+++ HowMuchMoney/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HowMuchMoney
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HowMuchMoney.Program.Main()");
+
+            Bank MyBank = new Bank( 73 );
+            int nmbrCoins = MyBank.CountMyCoins();
+            Console.WriteLine($"Wow! I have {nmbrCoins} coins in my piggybank!");
+
+            string[] allMyCoins = MyBank.GetCoins(nmbrCoins);
+            int howMuch = MyBank.HowMuch(allMyCoins);
+
+        }
+    }
+}
